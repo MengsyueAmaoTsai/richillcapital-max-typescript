36a189f11982afc3a188951f2dcaa46b16097456
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2023-01-29 04:27:17 +0800

    feat(MaxClient): getVipLevels() getVipLevel()

--- examples/market-data-client.ts
+++ examples/market-data-client.ts
@@ -1,2 +1,24 @@
+import MaxClient from '../richillcapital-max';
 
+const apiKey = process.env.API_KEY as string;
+const secretKey = process.env.SECRET_KEY as string;
+
+const client = new MaxClient(apiKey, secretKey);
+
+
+(async () => {
+    const vipLevels = await client.getVipLevels();
+    vipLevels.forEach(level => {
+        console.log(`
+Level: ${level.level} 
+MinTradeVolume: ${level.minStakingVolume}
+MakerFee: ${level.makerFee}
+TakerFee: ${level.takerFee}
+        `);
+    })
+
+
+    const vipLevel = await client.getVipLevel(5);
+    console.log(vipLevel);
+})();
 
--- richillcapital-max/index.ts
+++ richillcapital-max/index.ts
@@ -1,13 +1,91 @@
 
+const REST_URL = 'https://max-api.maicoin.com';
+
+interface VipLevel {
+    level: number;
+    minTradingVolume: number;
+    minStakingVolume: number;
+    makerFee: number;
+    takerFee: number;
+}
+
+interface VipLevelData {
+    level: number,
+    minimum_trading_volume: number,
+    minimum_staking_volume: number,
+    taker_fee: number,
+    maker_fee: number,    
+}
 
 class MaxClient {
-    private _apiKey: string;
-    private _secretKey: string;
+    protected _apiKey: string;
+    protected _secretKey: string;
 
-    constructor(apiKey: string, secretKey: string) {
+    public constructor(apiKey: string, secretKey: string) {
         this._apiKey = apiKey;
         this._secretKey = secretKey;
     }
+
+    //#region REST API Public
+
+    /**
+     * Get all vip level fees.
+     */
+    public getVipLevels = async (): Promise<VipLevel[]> => {
+        const endpoint = '/api/v2/vip_levels';
+
+        const url = REST_URL + endpoint;
+        const response = await fetch(url, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        
+        if (response.status !== 200) {
+            console.log(`Something went wrong on fetch()`);
+            return [] as VipLevel[]
+        } else {
+            const data = await response.json();
+            return data.map((item: VipLevelData) => {
+                return {
+                    level: item.level,
+                    minTradeVolume: item.minimum_trading_volume,
+                    minStakingVolume: item.minimum_staking_volume,
+                    takerFee: item.taker_fee,
+                    makerFee: item.maker_fee,
+                }
+            });
+        }
+    }
+
+    public getVipLevel = async (level: number): Promise<VipLevel | null> => {
+        const endpoint = `/api/v2/vip_levels/${level}`;
+
+        const url = REST_URL + endpoint;
+        const response = await fetch(url, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        
+        if (response.status !== 200) {
+            console.log(`Something went wrong on fetch()`);
+            return null;
+        } else {
+            const data: VipLevelData = await response.json();
+            return {
+                level: data.level,
+                minStakingVolume: data.minimum_staking_volume,
+                minTradingVolume: data.minimum_trading_volume,
+                takerFee: data.taker_fee,
+                makerFee: data.maker_fee 
+            }
+        }        
+    }
+
+    //#endregion
 }
 
 export default MaxClient;
